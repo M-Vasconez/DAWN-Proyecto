6e072fedd8a32a73e36d9e5aa45c14dc3cc2e6df
Matias Vasconez <68170693+M-Vasconez@users.noreply.github.com>
2022-08-14 22:57:49 -0500

    Add files via upload

---
+++ Proyecto 3/src/app/posiciones/posiciones.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posiciones',
+  templateUrl: './posiciones.page.html',
+  styleUrls: ['./posiciones.page.scss'],
+})
+export class PosicionesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
